266a3191cbb67811b6886cd874e044e30a29127a
zokaiG <futlouco7@gmail.com>
2018-10-10 14:58:30 -0300

    Upload tasks file for guideline.

---
+++ implementation_tasks.py
@@ -0,0 +1,9 @@
+"""Sequence of tasks left to tackle"""
+## TODO: Code Cost Function;
+## TODO: Invert "Input times weight... " and have WT.A in feedforward();
+## TODO: Code SGD: (Only minibatch) Leave Backprop for later;
+## TODO: Code Backprop;
+## TODO: Code a flexible network architecture;
+## TODO: Get the training, test, and validation data;
+## TODO: Tweak the hyper parameters and switch activation functions;
+## TODO: Start fighting overfitting: Apply droppout
