270ecb3c4b8fdb0e0418c6446b71d959880450ce
aluno <aluno@Micro01>
2018-10-16 13:48:01 -0300

    Refactored the code to accept different numbers of hidden layers.

--- vanillaNN.py
+++ vanillaNN.py
@@ -2,45 +2,49 @@ import numpy as np
 
 class VNN(object):
     """Just a simple barebone implementation of a vanilla neural network."""
-    def __init__(self,input,output, hidden):
-        self.input = input
-        self.output = output
-        self.hidden = hidden
-        self.weight = []
+    def __init__(self,layers):
+        self.inputLayer = layers[0]
+        self.outputLayer = layers[-1]
+        self.hiddenLayers = [layers[i] for i in range(1, len(layers)-1)]
+        self.weights = []
         self.bias = []
         self.initWeights()
         self.initBias()
 
     def initWeights(self):
-        self.weight.append(np.random.random((self.input.shape[1],self.hidden)))
-        self.weight.append(np.random.random((self.hidden,self.output)))
+        self.weights.append(np.random.random((self.hiddenLayers[0],self.inputLayer)))
+	for hL in range(1,len(self.hiddenLayers)):
+		self.weights.append(np.random.random((self.hiddenLayers[hL],self.hiddenLayers[hL-1])))
+        self.weights.append(np.random.random((self.outputLayer,self.hiddenLayers[-1])))
 
     def initBias(self):
-        self.bias.append(np.random.random((1,self.hidden)))
-        self.bias.append(np.random.random((1,self.output)))
+	for layer in self.hiddenLayers:
+	        self.bias.append(np.random.random((layer,1)))
+        self.bias.append(np.random.random((self.outputLayer,1)))
 
     def helloNN(self):
-        print("Input: {0}".format(self.input))
-        print("Output: {0}".format(self.output))
-        print("Hidden: {0}".format(self.hidden))
-        print("Weight: {0}".format(self.weight))
+        print("Input: {0}".format(self.inputLayer))
+        print("Output: {0}".format(self.outputLayer))
+        print("Hidden: {0}".format(self.hiddenLayers))
+        print("Weight: {0}".format(self.weights))
         print("bias: {0}".format(self.bias))
 
     def sigmoid(self,z):
         return 1.0/(1 + np.exp(-z))
 
     def feedforward(self, a):
-        for w, b in zip(self.weight, self.bias):
-            a = self.sigmoid(np.dot(a,w) + b)
+        for w, b in zip(self.weights, self.bias):
+            a = self.sigmoid(np.dot(w,a) + b)
         return a;
 
     def evaluate(self):
-        self.eval = self.feedforward(self.input)
+        self.eval = self.feedforward(self.inputLayer)
         print(self.eval)
 
-inputNumber = np.array([[1, 2, 3],[9, 8, 7]])
-outputNumber = 2
-hiddenNumber = 10
-vnn = VNN(inputNumber,outputNumber, hiddenNumber)
-vnn.helloNN()
+inputNumber = 784	#MNIST input
+hiddenNumber = 30	#Chosen architecture
+outputNumber = 10	#10 possible digits
+architecture = [inputNumber, hiddenNumber, outputNumber]
+vnn = VNN(architecture)
+#vnn.helloNN()
 vnn.evaluate()
